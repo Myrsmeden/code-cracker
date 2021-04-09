45188d07d0fe6916684d00dfb8c3890445da1002
Johan Myrsmeden <johan@myrsmeden.se>
2021-04-09 18:45:42 +0200

    Removing letter-only codes

--- functions/tasks/01-hex/hex.test.ts
+++ functions/tasks/01-hex/hex.test.ts
@@ -10,6 +10,7 @@ test('can decrypt', () => {
 
 test('can reverse', () => {
   const encrypted = task.indata.map(task.encrypt);
+  console.log(encrypted)
   const decrypted = encrypted.map(task.decrypt);
   decrypted.forEach((input) => expect(task.indata).toContain(input));
 });
--- functions/tasks/01-hex/hex.ts
+++ functions/tasks/01-hex/hex.ts
@@ -1,19 +1,8 @@
-import { Level } from '../../../src/shared/Task';
-
 export default {
   id: 1,
   encrypt: (unencrypted: string): string => parseInt(unencrypted, 10).toString(16),
   decrypt: (encrypted: string): string => parseInt(encrypted, 16).toString(10),
-  indata: [
-    '1044942',
-    '2343432205',
-    '3221229823',
-    '14600926',
-    '13286381',
-    '16435934',
-    '1027565',
-    '712173',
-  ],
+  indata: ['1044942', '2343432205', '3221229823'],
   information: [
     {
       html:
